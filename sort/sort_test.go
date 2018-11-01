3c797c7bdb66e46de954fa4dd6cbce169a93ee35
preminem <preminem@ruc.edu.cn>
2018-11-01 16:29:16 +0800

    added test model.

---
+++ sort/sort_test.go
@@ -0,0 +1,58 @@
+package sort
+
+import (
+	"testing"
+)
+
+func newValues() []int {
+	return []int{4, 93, 84, 85, 80, 37, 81, 93, 27, 12}
+}
+
+func BenchmarkChangeSort(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		ChangeSort(values)
+	}
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		BubbleSort(values)
+	}
+}
+
+func BenchmarkInsertSort(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		InsertSort(values)
+	}
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		QuickSort(values)
+	}
+}
+
+func BenchmarkMergeSortEntrance(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		MergeSortEntrance(values)
+	}
+}
+
+func BenchmarkShellSort(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		ShellSort(values)
+	}
+}
+
+func BenchmarkHeapSort(b *testing.B) {
+	values := newValues()
+	for i := 0; i < b.N; i++ {
+		HeapSort(values)
+	}
+}
