8d1115f9241ed96d91e6fd6eb84133c571fc7548
preminem <preminem@ruc.edu.cn>
2019-01-10 19:12:36 +0800

    fix bug

--- hash_map.go
+++ hash_map.go
@@ -24,16 +24,13 @@ type HashMap struct {
 
 //创建只有头结点的链表
 func CreateLink() *LinkNode {
-
 	//头结点数据为空 是为了标识这个链表还没有存储键值对
 	var linkNode = &LinkNode{KV{"", ""}, nil}
-
 	return linkNode
 }
 
 //尾插法添加节点,返回链表总长度
 func (link *LinkNode) AddNode(data KV) int {
-
 	var count = 0
 	//找到当前链表尾节点
 	tail := link
@@ -48,86 +45,73 @@ func (link *LinkNode) AddNode(data KV) int {
 			tail = tail.NextNode
 		}
 	}
-
 	var newNode = &LinkNode{data, nil}
 	tail.NextNode = newNode
-
 	return count + 1
 }
 
 //创建HashMap
 func CreateHashMap() *HashMap {
-
 	myMap := &HashMap{}
-
 	//为每个元素添加一个链表对象
 	for i := 0; i < BucketCount; i++ {
-
 		myMap.Buckets[i] = CreateLink()
 	}
-
 	return myMap
 }
 
 //自定义一个简单的散列算法，它可以将不同长度的key散列成0-BucketCount的整数
 func HashCode(key string) int {
-
 	var sum = 0
 	for i := 0; i < len(key); i++ {
-
+		//字符串中每个字符都会对应一个整数
 		sum += int(key[i])
 	}
-
 	return (sum % BucketCount)
 }
 
 //添加键值对
 func (myMap *HashMap) AddKeyValue(key string, value string) {
-
 	//1.将key散列成0-BucketCount的整数作为Map的数组下标
 	var mapIndex = HashCode(key)
-
 	//2.获取对应数组头结点
 	var link = myMap.Buckets[mapIndex]
-
 	//3.在此链表添加结点
 	if link.Data.Key == "" && link.NextNode == nil {
 		//如果当前链表只有一个节点，说明之前未有值插入  修改第一个节点的值 即未发生哈希碰撞
 		link.Data.Key = key
 		link.Data.Value = value
-
-		fmt.Printf("node key:%v add to buckets %d first node\n", key, mapIndex)
 	} else {
-
 		//发生哈希碰撞
 		index := link.AddNode(KV{key, value})
-		fmt.Printf("node key:%v add to buckets %d %dth node\n", key, mapIndex, index)
 	}
 }
 
 //按键取值
-func (myMap *HashMap) GetValueForKey(key string) string {
-
+func (myMap *HashMap) GetValueForKey(key string) string,bool {
 	//1.将key散列成0-BucketCount的整数作为Map的数组下标
 	var mapIndex = HashCode(key)
-
 	//2.获取对应数组头结点
 	var link = myMap.Buckets[mapIndex]
-
 	var value string
-
 	//遍历找到key对应的节点
 	head := link
 	for {
+		if head == nil{
+			return nil,false
+		}
 		if head.Data.Key == key {
-
 			value = head.Data.Value
-			break
+			return value,true
 		} else {
 
 			head = head.NextNode
 		}
 	}
+}
 
-	return value
+func (myMap *HashMap) DeleteValueForKey(key string) {
+  var mapIndex = HashCode(key)
+  //头节点指向下个节点的指针置空
+  myMap.Buckets[mapIndex].NextNode = nil
 }
