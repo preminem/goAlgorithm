7f9f35cf259e48378796c1d994f983195a5bf792
preminem <preminem@ruc.edu.cn>
2019-02-11 23:38:59 +0800

    add 10 problem

---
+++ sword/10.fibonacci/fibonacci.go
@@ -0,0 +1,28 @@
+package problem10
+
+/*
+题：
+求斐波那契数列第n项
+
+分析：
+递归不是最佳解法，效率及其低下。
+最佳解决办法很简单，用f(0)f(1)算出f(2)，在f(1)f(2)算出f(3)..依次类推算出f(n)
+*/
+
+func fib(num int) int {
+	if num == 0 {
+		return 0
+	} else if num == 1 {
+		return 1
+	}
+
+	fibNMinusOne := 1
+	fibNMinusTwo := 0
+	fibN := 0
+	for i := 2; i <= num; i++ {
+		fibN = fibNMinusOne + fibNMinusTwo
+		fibNMinusTwo = fibNMinusOne
+		fibNMinusOne = fibN
+	}
+	return fibN
+}
