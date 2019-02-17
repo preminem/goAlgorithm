4b5054c3c2f1e6122dea049c6b994a2ec5b5c730
preminem <preminem@ruc.edu.cn>
2019-02-17 23:43:03 +0800

    add 18 problem

---
+++ leet-code/0018.4sum/4sum.go
@@ -0,0 +1,79 @@
+package problem0018
+
+/*
+Topic:
+给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？
+找出所有满足条件且不重复的四元组。
+注意：答案中不可以包含重复的四元组。
+
+Example:
+给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+满足要求的四元组集合为：
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+
+Thought:
+和15题思路一样，多一层循环。
+*/
+
+import (
+	"sort"
+)
+
+func fourSum(nums []int, target int) [][]int {
+	res := [][]int{}
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-3; i++ {
+		// 避免添加重复的结果
+		// i>0 是为了防止nums[i-1]溢出
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		for j := i + 1; j < len(nums)-2; j++ {
+			// 避免添加重复的结果
+			// nums[j-1]虽然不会溢出
+			// 但是不添加 j > i+1 的话，会漏掉那些i和j为相同数值的答案
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+
+			l, r := j+1, len(nums)-1
+			for l < r {
+				s := nums[i] + nums[j] + nums[l] + nums[r]
+				switch {
+				case s < target:
+					l++
+				case s > target:
+					r--
+				default:
+					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
+					l, r = next(nums, l, r)
+				}
+			}
+		}
+
+	}
+	return res
+}
+
+func next(nums []int, l, r int) (int, int) {
+	for l < r {
+		switch {
+		case nums[l] == nums[l+1]:
+			l++
+		case nums[r] == nums[r-1]:
+			r--
+		default:
+			l++
+			r--
+			return l, r
+		}
+	}
+
+	return l, r
+}
