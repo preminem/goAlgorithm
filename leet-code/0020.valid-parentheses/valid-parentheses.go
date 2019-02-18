30a88fc5d103ede06268d8bc01876e0194f68bf6
preminem <preminem@ruc.edu.cn>
2019-02-18 23:58:46 +0800

    add 20 problem

---
+++ leet-code/0020.valid-parentheses/valid-parentheses.go
@@ -0,0 +1,65 @@
+package problem0020
+
+/*
+Topic:
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+有效字符串需满足：
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串
+
+Example:
+Example 1:
+Input: "()"
+Output: true
+
+Example 2:
+Input: "()[]{}"
+Output: true
+
+Example 3:
+Input: "(]"
+Output: false
+
+Example 4:
+Input: "([)]"
+Output: false
+
+Example 5:
+Input: "{[]}"
+Output: true
+
+Thought:
+初始化栈 S。
+一次处理表达式的每个括号。
+如果遇到开括号，我们只需将其推到栈上即可。这意味着我们将稍后处理它，让我们简单地转到前面的 子表达式。
+如果我们遇到一个闭括号，那么我们检查栈顶的元素。如果栈顶的元素是一个 相同类型的 左括号，那么我们将它从栈中弹出并继续处理。否则，这意味着表达式无效。
+如果到最后我们剩下的栈中仍然有元素，那么这意味着表达式无效。
+*/
+
+func isValid(s string) bool {
+	size := len(s)
+
+	stack := make([]byte, size)
+	top := 0
+
+	for i := 0; i < size; i++ {
+		c := s[i]
+		switch c {
+		case '(':
+			stack[top] = c + 1 // '('+1 is ')'
+			top++
+		case '[', '{':
+			stack[top] = c + 2
+			top++
+		case ')', ']', '}':
+			if top > 0 && stack[top-1] == c {
+				top--
+			} else {
+				return false
+			}
+		}
+	}
+
+	return top == 0
+}
