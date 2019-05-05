fc7295db24912f0c38bb3009e58be44454e6a6f3
preminem <preminem@ruc.edu.cn>
2019-05-05 19:26:55 +0800

    added 0053 problem

---
+++ leet-code/0053.maximum-subarray/maximum-subarray.go
@@ -0,0 +1,30 @@
+/*
+题：最大子序和
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+示例:
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+进阶:
+如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+*/
+package problem0053
+
+func maxSubArray(nums []int) int {
+	sum, maxSum := -1<<31, -1<<31
+	for _, n := range nums {
+		// sum+n < n，那就还不如直接从 n 开始统计。
+		sum = max(sum+n, n)
+		maxSum = max(maxSum, sum)
+	}
+	return maxSum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
